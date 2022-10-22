2efeaa4a8c561458dd81c57f2beb4a10bee15a16
Moril <jeanmo333@gmail.com>
2022-10-22 00:21:10 -0300

    commit

--- components/admin/AdminNavbar.tsx
+++ components/admin/AdminNavbar.tsx
@@ -1,33 +1,43 @@
-import { useContext } from 'react';
-import NextLink from 'next/link';
+import { useContext } from "react";
+import NextLink from "next/link";
 
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Link,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import DehazeIcon from "@mui/icons-material/Dehaze";
 
-import { AppBar, Box, Button, Link, Toolbar, Typography } from '@mui/material';
-
-
-import { UiContext } from '../../context';
+import { UiContext } from "../../context";
 
 export const AdminNavbar = () => {
+  const { toggleSideMenu } = useContext(UiContext);
 
-    const { toggleSideMenu } = useContext( UiContext );
-    
+  return (
+    <AppBar>
+      <Toolbar>
+        <NextLink href="/" passHref>
+          <Link display="flex" alignItems="center">
+            <Typography
+              variant="h6"
+              sx={{ fontWeight: "bold", fontSize: 25, color: "white" }}>
+              Shopi
+            </Typography>
+          </Link>
+        </NextLink>
 
-    return (
-        <AppBar>
-            <Toolbar>
-                <NextLink href='/' passHref>
-                    <Link display='flex' alignItems='center'>
-                    <Typography variant="h6" sx={{ fontWeight: "bold", fontSize: 25, color: "white"   }}>Shopi</Typography>
-                    </Link>  
-                </NextLink>
+        <Box flex={1} />
 
-                <Box flex={ 1 } />
-
-                <Button onClick={ toggleSideMenu }>
-                    Menú
-                </Button>
-
-            </Toolbar>
-        </AppBar>
-    )
-}
+        <Box sx={{ display: { xs: "block", sm: "block" } }}>
+          <IconButton sx={{ marginRight: 2, color: "white" }}>
+            <DehazeIcon onClick={toggleSideMenu} />
+          </IconButton>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
